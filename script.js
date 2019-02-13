d8fbf4739bca0be23228397e1e07f9efe1ebaaab
Duc <duc.h@hotmail.com>
2019-02-13 19:34:43 +1100

    Added all autoclickers and onload function

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-/* jslint devel: true */
-/* browser: true */
 var amount = 0;
 var autoclicker1= 0;
+var autoclicker2 = 0;
+var autoclicker3 = 0;
 
 function update() {
-    document.getElementById('amountText').value = Math.trunc(amount);
-    document.title = Math.trunc(amount);
-    document.getElementById('amountAutoText').value = autoclicker1;
-}
-
-function changePrices() {
-    autoClickers[1][2] = 12 + (autoclicker1 * 2);
-    document.getElementById("debug1").innerHTML = "Current Price of AutoClicker1: " + autoClickers[1][2];
+    document.getElementById('amountText').value = Math.trunc(amount);   //updates number in input box, title of website, and
+    document.title = "Weaboo Simulator: " + Math.trunc(amount) + " Animes";
+    document.getElementById('amountAutoClicker1Text').value = autoclicker1;
+    document.getElementById('amountAutoClicker2Text').value = autoclicker2;
+    document.getElementById('amountAutoClicker3Text').value = autoclicker3;
 }
 
 function timer() {
-    amount += (autoclicker1 * 0.1);
+    amount += (autoclicker1 * 0.003);
+    amount += (autoclicker2 * 0.006);
+    amount += (autoclicker3 * 0.009);
     update();
 }
-setInterval(timer, 100);
+setInterval(timer, 10);
 
-var autoClickers = [
+var autoClickers = [    //first array accesses which autoclicker , second array accesses properties of that autoclicker (e.g. qty, current price, initial price)
 ["name", "initialPrice", "currentPrice", "qty"],
-["first", 12, 12, 0],
-["name2", "initialPrice", "currentPrice", "qty"],
-["name3", "initialPrice", "currentPrice", "qty"],
+["AutoClicker1", 12, 12, 0],
+["AutoClicker2", 84, 84, 0],
+["AutoClicker3", 166, 166, 0],
 ["name4", "initialPrice", "currentPrice", "qty"],
 ["name5", "initialPrice", "currentPrice", "qty"]
 ]
@@ -37,15 +36,33 @@ function Increment() {
 function save() {
     localStorage.setItem("Amount of Animes", amount);
     localStorage.setItem("Amount of AutoClicker1's", autoclicker1);
+    localStorage.setItem("Amount of AutoClicker2's", autoclicker2);
+    localStorage.setItem("Amount of AutoClicker3's", autoclicker3);
 }
 
 function load() {
     amount = localStorage.getItem("Amount of Animes");
     amount = parseInt(amount);
+
     autoclicker1 = localStorage.getItem("Amount of AutoClicker1's");
     autoclicker1 = parseInt(autoclicker1);
+    autoclicker2 = localStorage.getItem("Amount of AutoClicker2's");
+    autoclicker2 = parseInt(autoclicker2);
+    autoclicker3 = localStorage.getItem("Amount of AutoClicker3's");
+    autoclicker3 = parseInt(autoclicker3);
+
     update();
-    changePrices();
+    changePricesOnLoad();
+}
+
+function changePricesOnLoad() {
+    autoClickers[1][2] = 12 + (autoclicker1 * 2);
+    autoClickers[2][2] = 84 + (autoclicker2 * 10);
+    autoClickers[3][2] = 166 + (autoclicker3 * 40)
+
+    document.getElementById("debug1").innerHTML = "Current Price of AutoClicker1: " + autoClickers[1][2];
+    document.getElementById("debug2").innerHTML = "Current Price of AutoClicker2: " + autoClickers[2][2];
+    document.getElementById("debug3").innerHTML = "Current Price of AutoClicker3: " + autoClickers[3][2];
 }
 
 function buyAutoClicker1() {
@@ -59,14 +76,35 @@ function buyAutoClicker1() {
     }
 }
 
-function buyAutoClicker() {
-    if (amount >= 12) {
-        amount = amount - 12;
-        autoclicker1 += 1;
+function buyAutoClicker2() {
+    if (amount >= autoClickers[2][2]) {
+        amount = amount - autoClickers[2][2];
+        autoClickers[2][3] += 1;
+        autoClickers[2][2] += 10;
+        autoclicker2 += 1;
         update();
+        document.getElementById("debug2").innerHTML = "Current Price of AutoClicker2: " + autoClickers[2][2];
+    }
+}
+
+function buyAutoClicker3() {
+    if (amount >= autoClickers[3][2]) {
+        amount = amount - autoClickers[3][2];
+        autoClickers[3][3] += 1;
+        autoClickers[3][2] += 40;
+        autoclicker3 += 1;
+        update();
+        document.getElementById("debug3").innerHTML = "Current Price of AutoClicker3: " + autoClickers[3][2];
     }
 }
 
 function welcomeBack() {
-    alert("welcome back!");
+    if (localStorage.getItem("Amount of Animes") === null) {
+        window.alert("Welcome to Matt's Waifu Clicker! owo");
+    }
+    else {
+        window.alert("Welcome back Onii-Chan! uwu");
+    }
 }
+
+window.onload = welcomeBack();
