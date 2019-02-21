bba7788600132ded655531e7e75516e878b96978
Duc <duc.h@hotmail.com>
2019-02-22 00:26:01 +1100

    Skill1 can auto unlock from meeting requirement (look at checkSkillReq function)

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ var IsekaiAmount = 0;
 var MoneyAmount = 0;
 
 var jobArray = [ //first array accesses which job. [1]=Initial$, [2]=Current$, [3]=Job Qty, [4]=Amount job increments by per tick
-["name", "initialPrice", "currentPrice", "upgrade", "effect", "desc", "1$$2"], //skills use $$ as breaks
-["Degenerate", 12, 0, 0, 0.003, "meme", "1$$2$$4"],
-["Tendies chef", 84, 0, 0, 0.006, "meme2"],
-["Chronic masterbaiter", 166, 0, 0, 0.009, "meme3"],
-["Hentai reviewer", 300, 0, 0, 0.015, "meme4"],
-["Body pillow stuffer", 600, 0, 0, 0.040, "meme5"],
-["Chronic masterbaiter", 1660, 0, 0, 0.009, "meme3"],
-["Hentai reviewer", 3000, 0, 0, 0.015, "meme4"],
-["Body pillow stuffer", 6000, 0, 0, 0.040, "meme5"],
-["Chronic masterbaiter", 1606, 0, 0, 0.009, "meme3"],
-["Hentai reviewer", 30000, 0, 0, 0.015, "meme4"],
-["Body pillow stuffer", 60000, 0, 0, 0.040, "meme5"],
+  ["name", "initialPrice", "currentPrice", "upgrade", "effect", "desc", "1$$2"], //skills use $$ as breaks
+  ["Degenerate", 12, 0, 0, 0.003, "meme", "1$$2$$4"],
+  ["Tendies chef", 84, 0, 0, 0.006, "meme2"],
+  ["Chronic masterbaiter", 166, 0, 0, 0.009, "meme3"],
+  ["Hentai reviewer", 300, 0, 0, 0.015, "meme4"],
+  ["Body pillow stuffer", 600, 0, 0, 0.040, "meme5"],
+  ["Chronic masterbaiter", 1660, 0, 0, 0.009, "meme3"],
+  ["Hentai reviewer", 3000, 0, 0, 0.015, "meme4"],
+  ["Body pillow stuffer", 6000, 0, 0, 0.040, "meme5"],
+  ["Chronic masterbaiter", 1606, 0, 0, 0.009, "meme3"],
+  ["Hentai reviewer", 30000, 0, 0, 0.015, "meme4"],
+  ["Body pillow stuffer", 60000, 0, 0, 0.040, "meme5"],
 ]
 
-var skillArray = [ //jobs have skill requirements which will be checked per tick
+var skillArray = [ //jobs have skill requirements which will be checked per tick. First array accesses which skill. [1]=Requirement, [2]=boolean if unlocked, [3]=description.
   ["name", "animeReq", "unlocked", "desc"],
-  ["Ninja", 10, false, "meme"],
+  ["Ninja", "20-Shounen,5-Isekai", false, "meme"], //For test purposes! 10 Shounen required.
   ["Japanese literacy", 20, false, "meme2"],
   ["Chronic masterbaiter", 30, false, "meme3"],
   ["Hentai reviewer", 40, false, "meme4"],
@@ -29,12 +29,12 @@ var skillArray = [ //jobs have skill requirements which will be checked per tick
 ]
 
 var merchArray = [ //first array accesses which job. [1]=Initial, [2]=CurrentPrice, [3]=Job Qty, [4]=Amount job increments by per tick
-["name", "initialPrice", "currentPrice", "qty", "effect", "desc"], //todo: add skill requirements
-["Key Chain", 10, 0, 0, 0.003, "meme"],
-["Wall scroll", 25, 0, 0, 0.006, "meme2"],
-["Megumin figure", 60, 0, 0, 0.009, "meme3"],
-["Love live onahole", 200, 0, 0, 0.015, "meme4"],
-["1:1 scale shiro", 800, 0, 0, 0.040, "meme5"],
+  ["name", "initialPrice", "currentPrice", "qty", "effect", "desc"], //todo: add skill requirements
+  ["Key Chain", 10, 0, 0, 0.003, "meme"],
+  ["Wall scroll", 25, 0, 0, 0.006, "meme2"],
+  ["Megumin figure", 60, 0, 0, 0.009, "meme3"],
+  ["Love live onahole", 200, 0, 0, 0.015, "meme4"],
+  ["1:1 scale shiro", 800, 0, 0, 0.040, "meme5"],
 ]
 
 function debug() {
@@ -45,49 +45,47 @@ function debug() {
 
 function loadNames() {
   createJobDivsCreateElement();
-createMerchDivsCreateElement()
-  for (i = 1; i < jobArray.length; i++)
-  {
+  createMerchDivsCreateElement()
+  for (i = 1; i < jobArray.length; i++) {
     document.getElementById('job' + i + 'Title').innerHTML = jobArray[i][0];
   }
 
-  for (i = 1; i < merchArray.length; i++)
-  {
+  for (i = 1; i < merchArray.length; i++) {
     document.getElementById('merch' + i + 'Title').innerHTML = merchArray[i][0];
   }
 
 }
+
 function createJobDivsCreateElement() {
-  for (i = 1; i < jobArray.length; i++)
-  {
+  for (i = 1; i < jobArray.length; i++) {
     var jobsDivJS = document.getElementById('jobs');
 
     var innerJobDivJS = document.createElement('div');
-    innerJobDivJS.setAttribute('class','innerJobDiv');
+    innerJobDivJS.setAttribute('class', 'innerJobDiv');
 
     var jobImgJS = document.createElement('img');
-    jobImgJS.setAttribute('class','imgJobSmall');
+    jobImgJS.setAttribute('class', 'imgJobSmall');
 
     var jobTitleJS = document.createElement('div');
-    jobTitleJS.setAttribute('class','jobTitle');
+    jobTitleJS.setAttribute('class', 'jobTitle');
 
     var jobReqJS = document.createElement('div');
-    jobReqJS.setAttribute('class','jobReq');
+    jobReqJS.setAttribute('class', 'jobReq');
 
     var jobQtyJS = document.createElement('div');
-    jobQtyJS.setAttribute('class','jobQty');
+    jobQtyJS.setAttribute('class', 'jobQty');
 
 
 
-    jobTitleJS.setAttribute('id','job' + i + 'Title');
+    jobTitleJS.setAttribute('id', 'job' + i + 'Title');
 
-    jobReqJS.setAttribute('id','jobCost' + i);
+    jobReqJS.setAttribute('id', 'jobCost' + i);
 
-    jobQtyJS.setAttribute('id','jobQty' + i);
+    jobQtyJS.setAttribute('id', 'jobQty' + i);
 
-    innerJobDivJS.setAttribute('onClick','buyJob(' + i + ')');
+    innerJobDivJS.setAttribute('onClick', 'buyJob(' + i + ')');
 
-    jobImgJS.setAttribute('src','media/ph' + i + '.png');
+    jobImgJS.setAttribute('src', 'media/ph' + i + '.png');
 
     innerJobDivJS.appendChild(jobImgJS);
     innerJobDivJS.appendChild(jobTitleJS);
@@ -98,36 +96,35 @@ function createJobDivsCreateElement() {
 }
 
 function createMerchDivsCreateElement() {
-  for (i = 1; i < merchArray.length; i++)
-  {
+  for (i = 1; i < merchArray.length; i++) {
     var jobsDivJS = document.getElementById('merch');
 
     var innerJobDivJS = document.createElement('div');
-    innerJobDivJS.setAttribute('class','innerJobDiv');
+    innerJobDivJS.setAttribute('class', 'innerJobDiv');
 
     var jobImgJS = document.createElement('img');
-    jobImgJS.setAttribute('class','imgJobSmall');
+    jobImgJS.setAttribute('class', 'imgJobSmall');
 
     var jobTitleJS = document.createElement('div');
-    jobTitleJS.setAttribute('class','jobTitle');
+    jobTitleJS.setAttribute('class', 'jobTitle');
 
     var jobReqJS = document.createElement('div');
-    jobReqJS.setAttribute('class','jobReq');
+    jobReqJS.setAttribute('class', 'jobReq');
 
     var jobQtyJS = document.createElement('div');
-    jobQtyJS.setAttribute('class','jobQty');
+    jobQtyJS.setAttribute('class', 'jobQty');
 
 
 
-    jobTitleJS.setAttribute('id','merch' + i + 'Title');
+    jobTitleJS.setAttribute('id', 'merch' + i + 'Title');
 
-    jobReqJS.setAttribute('id','merchCost' + i);
+    jobReqJS.setAttribute('id', 'merchCost' + i);
 
-    jobQtyJS.setAttribute('id','merchQty' + i);
+    jobQtyJS.setAttribute('id', 'merchQty' + i);
 
-    innerJobDivJS.setAttribute('onClick','buyMerch(' + i + ')');
+    innerJobDivJS.setAttribute('onClick', 'buyMerch(' + i + ')');
 
-    jobImgJS.setAttribute('src','media/ph' + i + '.png');
+    jobImgJS.setAttribute('src', 'media/ph' + i + '.png');
 
     innerJobDivJS.appendChild(jobImgJS);
     innerJobDivJS.appendChild(jobTitleJS);
@@ -139,22 +136,21 @@ function createMerchDivsCreateElement() {
 
 function update() {
   updatePrices();
+
   document.getElementById("ShounenAmountText").value = ShounenAmount.toFixed(0); //can't make dynamic with genre.toFixed(0). Need to figure out how to use the argument as a reference to the variable.
   document.getElementById("RomanceAmountText").value = RomanceAmount.toFixed(0);
   document.getElementById("SliceOfLifeAmountText").value = SliceOfLifeAmount.toFixed(0);
   document.getElementById("IsekaiAmountText").value = IsekaiAmount.toFixed(0);
 
   document.getElementById('MoneyAmountText').value = "$" + MoneyAmount.toFixed(2);
-  // document.title = "Weeaboo Simulator: " + Math.trunc(AnimeAmount) + " Animes"; a bit obsolete since we use both animes and money, might as well keep the page title as just 'Weaboo Simulator'
-  for (i = 1; i < jobArray.length; i++)
-  {
-    document.getElementById('jobQty'+i).innerHTML = jobArray[i][3];
+  for (i = 1; i < jobArray.length; i++) {
+    document.getElementById('jobQty' + i).innerHTML = jobArray[i][3];
   }
 
-  for (i = 1; i < jobArray.length; i++)
-  {
-    document.getElementById('merchQty'+i).innerHTML = merchArray[i][3];
+  for (i = 1; i < merchArray.length; i++) {
+    document.getElementById('merchQty' + i).innerHTML = merchArray[i][3];
   }
+  checkSkillReq();
 }
 
 function timer() {
@@ -169,46 +165,89 @@ function timer() {
 }
 setInterval(timer, 100);
 
-function unlockSkill(i) {
-  if (skillArray[i][1] <= AnimeAmount) {
-    skillArray[i][2] = true;
-    // AnimeAmount -= skillArray[i][2]; skills dont cost anime
-    document.getElementById("Skill" + i + "Req").innerHTML = "UNLOCKED";
-    document.getElementById("Skill" + i + "Div").style.pointerEvents = 'none';
-    //          <div class="innerJobDiv" id="Skill1Div" onclick="unlockSkill()">
-    //document.getElementById('debug1skill').innerHTML = "UNLOCKED"; DEBUG (although it would be a nice feature, TODO in future)
+// function unlockSkill(i) {
+//   if (skillArray[i][1] <= AnimeAmount) {
+//     skillArray[i][2] = true;
+//     // AnimeAmount -= skillArray[i][2]; skills dont cost anime
+//     document.getElementById("Skill" + i + "Req").innerHTML = "UNLOCKED";
+//     document.getElementById("Skill" + i + "Div").style.pointerEvents = 'none';
+//     //          <div class="innerJobDiv" id="Skill1Div" onclick="unlockSkill()">
+//     //document.getElementById('debug1skill').innerHTML = "UNLOCKED"; DEBUG (although it would be a nice feature, TODO in future)
+//   }
+
+// }
+
+function checkSkillReq() { //Note: Create method called 'loadSkills()' for future.
+  for (i = 1; i < skillArray.length; i++) //loops 5 times currently.
+  {
+    var requiredCounter = 0; //used to count if player has the required amount for a skill (look at switch statement for use).
+    if (skillArray[i][2] == true) {
+      continue; //no need to check for skills that are already unlocked
+    }
+    var totalSkillReqs = skillArray[i][1];
+    var genres = totalSkillReqs.split(",");
+
+    for (x = 0; x < genres.length; x++) //loops through the genres array. E.g. for 10-Shounen,5-Isekai, it will loop twice.
+    {
+
+      switch (genres[x].substring(genres[x].indexOf("-") + 1)) //E.g. for 10-Shounen, the switch argument is "Shounen".
+      {
+        case ('Shounen'):
+          var numberReq = parseInt(genres[x].substring(0, genres[x].indexOf("-"))) //E.g. for 10-Shounen, number = 10;
+          if (ShounenAmount >= numberReq) {
+            requiredCounter += 1;
+          }
+          break;
+
+        case ('Romance'):
+          var numberReq = parseInt(genres[x].substring(0, genres[x].indexOf("-"))) 
+          if (RomanceAmount >= numberReq) {
+            requiredCounter += 1;
+          }
+          break;
+
+        case ('SliceOfLife'):
+          var numberReq = parseInt(genres[x].substring(0, genres[x].indexOf("-")))
+          if (SliceOfLifeAmount >= numberReq) {
+            requiredCounter += 1;
+          }
+          break;
+
+        case ('Isekai'):
+          var numberReq = parseInt(genres[x].substring(0, genres[x].indexOf("-"))) 
+          if (IsekaiAmount >= numberReq) {
+            requiredCounter += 1;
+          }
+          break;
+      }
+
+      if (requiredCounter == genres.length) //If the counter = genres.length, it means all genres have met their requirement and thus, the skill is unlocked.
+      {
+        skillArray[i][2] = true;
+        document.getElementById("Skill" + i + "Req").innerHTML = skillArray[i][3];
+      }
+    }
   }
-
 }
-
-// function checkSkillReq() {
-//   for (i = 1; i < skillArray.length; i++)
-//   {
-//   var skillCheck = skillArray[i][1];
-//   var textByLine = text.split("\n")
-//
-//
-// } }
 
 
 function Increment(genre) {
-  switch(genre)
-  {
+  switch (genre) {
     case ('ShounenAmount'):
-    ShounenAmount++;
-    break;
+      ShounenAmount++;
+      break;
 
-    case('RomanceAmount'):
-    RomanceAmount++;
-    break;
+    case ('RomanceAmount'):
+      RomanceAmount++;
+      break;
 
-    case('SliceOfLifeAmount'):
-    SliceOfLifeAmount++;
-    break;
+    case ('SliceOfLifeAmount'):
+      SliceOfLifeAmount++;
+      break;
 
-    case('IsekaiAmount'):
-    IsekaiAmount++;
-    break;
+    case ('IsekaiAmount'):
+      IsekaiAmount++;
+      break;
   }
   update();
 }
@@ -239,16 +278,14 @@ function load() {
 
 function updatePrices() {
 
-  for (i = 1; i < jobArray.length; i++)
-  {
+  for (i = 1; i < jobArray.length; i++) {
     jobArray[i][2] = jobArray[i][1] + (jobArray[i][3] * jobArray[i][1] * 0.2);
-    document.getElementById("jobCost"+i).innerHTML = jobArray[i][2];
+    document.getElementById("jobCost" + i).innerHTML = jobArray[i][2];
   }
 
-  for (i = 1; i < merchArray.length; i++)
-  {
+  for (i = 1; i < merchArray.length; i++) {
     merchArray[i][2] = merchArray[i][1] + (merchArray[i][3] * merchArray[i][1] * 0.2);
-    document.getElementById("merchCost"+i).innerHTML = merchArray[i][2];
+    document.getElementById("merchCost" + i).innerHTML = merchArray[i][2];
   }
 }
 
@@ -269,7 +306,6 @@ function buyMerch(i) {
 }
 
 function changeWaifu(Waifu) {
-  //<img id="CurrentWaifuImg" src="media/futaba.PNG">
   document.getElementById("CurrentWaifuDiv").innerHTML = '<img src="media/' + Waifu + '"> <br/>' + Waifu.split('.')[0];
 }
 
